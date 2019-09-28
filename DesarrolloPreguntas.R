024791fb6cce2a02ec97d7a7dd1c8d3afc7844a3
Dabor96 <dabor.olatel@utem.cl>
2019-09-28 11:18:22 -0300

    Se sube el script

---
+++ DesarrolloPreguntas.R
@@ -0,0 +1,30 @@
+listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+listaDeNumeros[5]
+listaDeNumeros[0]
+listaDeNumeros[-5]
+listaDeNumeros[14]
+
+unlist(listaDeNumeros[5]) 
+if(listaDeNumeros[5]+1>0){
+  print("se cumple")
+}
+
+if(unlist(listaDeNumeros[5])+1>0){
+  print("se cumple")
+}
+
+listaDeNumeros[5] <- 12
+length(nombre_variable)
+length(listaDeNumeros)
+valorInicial <- 5
+valorFinal <- 20
+valorInicial : valorFinal
+valorInicial <- 20
+valorFinal <- 5
+valorInicial : valorFinal
+length(listaDeNumeros) : valorFinal
+length(listaDeNumeros) : valorFinal
+valorInicial : length(listaDeNumeros)
+
+valorInicial <- 20
+valorFinal <- 20
